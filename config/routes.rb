ae0a3ace5d3c4f4398bbd25544a76b1af2a01790
Ryuya3871 <ryuya-3@outlook.jp>
2024-08-03 22:53:14 +0900

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'   #ここを追記します
   get 'lists' => 'lists#index'
   get 'lists/show'
-  get 'lists/edit'
   get 'top' => 'homes#top'
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
